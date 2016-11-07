dc58e19b5e06620d862cb27a74865f654748e0f4
Alex Lalejini <amlalejini@gmail.com>
2016-11-07 18:43:22 -0500

    Wrote script to assign an ordering to the clusters such that they will make nice trees when drawn. In the process of making tree branches prettier.

---
+++ scripts/order_cluster_data.py
@@ -0,0 +1,54 @@
+"""
+This script orders cluster data to maximize prettiness it look pretty for visualization.
+"""
+
+import sys, os
+
+def main():
+    cluster_data = sys.argv[1]
+
+    with open(cluster_data, "r") as fp:
+        content = fp.read().strip()
+        content = content.split("\n")
+        attrs = content[0].split(",")
+        content = content[1:]
+    print attrs
+    time_slices = {}
+    times = set([])
+    for line in content:
+        cluster_info = {attrs[i]:line.split(",")[i] for i in range(0, len(attrs))}
+        time = int(cluster_info["cluster_id"].split("_")[0])
+        if not time in time_slices:
+            time_slices[time] = []
+        time_slices[time].append(cluster_info)
+        times.add(time)
+    times = list(times)
+    times.sort()
+
+    # For each time slice: assign an ordering to each cluster in that timeslice.
+    ordering_lookup = {}
+    order_cnt = 0
+    for ti in range(0, len(times)):
+        if ti == 0:
+            for cluster in time_slices[times[ti]]:
+                ordering_lookup[cluster["cluster_id"]] = order_cnt
+                order_cnt += 1
+        else:
+            # Sort clusters by back cluster id
+            clusters = [(ordering_lookup[cluster["back_cluster_id"]], cluster) for cluster in time_slices[times[ti]]]
+            clusters.sort() # Sort on back_cluster_id
+            for cluster in clusters:
+                ordering_lookup[cluster[1]["cluster_id"]] = order_cnt
+                order_cnt += 1
+
+    # Output cluster data file with ordering information.
+    new_content = ",".join(attrs) + ",order\n"
+    for line in content:
+        cluster_info = {attrs[i]:line.split(",")[i] for i in range(0, len(attrs))}
+        new_content += line + "," + str(ordering_lookup[cluster_info["cluster_id"]]) + "\n"
+    new_content = new_content.strip("\n")
+    with open("ordered_cluster_data.csv", "w") as fp:
+        fp.write(new_content)
+
+if __name__ == "__main__":
+    main()
